a02f4c4ada5b7c519528b0cda28f4e69c0eacd84
Myroslav Zapukhlyak <zapukhlyak@gmail.com>
2015-10-26 17:21:54 +0100

    fix comments
    Fixed typos in comments.
    
    Signed-off-by: Myroslav Zapukhlyak <zapukhlyak@gmail.com>

--- design_patterns/strategy_pattern.f90
+++ design_patterns/strategy_pattern.f90
@@ -1,4 +1,4 @@
-!Copyright (c) 2015 zmiimz
+!Copyright (c) 2015 M. Zapukhlyak
 !THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 !IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
 !FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
@@ -7,7 +7,7 @@
 !CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 !-----------------------------------------------------------------------
-!Module strategy_module
+!Module strategy_module implements strategy design pattern
 !-----------------------------------------------------------------------
 
 module strategy_module
